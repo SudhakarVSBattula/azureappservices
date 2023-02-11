67153c2ba9e4da3f6e6e9875af20e68d276180ed
SudhakarVSBattula <sudhakarbvs@outlook.com>
2023-02-11 21:30:53 +0530

    changed app resource group name

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "terraform_app_service_rg" {
-  name     = "terraform-app-service-rg"
+resource "azurerm_resource_group" "terraform_appservice_rg" {
+  name     = "terraform-appservice-rg"
   location = var.location
 }
 
 resource "azurerm_service_plan" "terraform_app_service_plan" {
   name                = "terraform-app-service-plan"
-  location            = azurerm_resource_group.terraform_app_service_rg.location
-  resource_group_name = azurerm_resource_group.terraform_app_service_rg.name
+  location            = azurerm_resource_group.terraform_appservice_rg.location
+  resource_group_name = azurerm_resource_group.terraform_appservice_rg.name
   os_type             = "Windows"
   sku_name            = "S1"
 }
 
 resource "azurerm_windows_web_app" "terraform_app_service" {
   name                = "app-service-90961519"
-  location            = azurerm_resource_group.terraform_app_service_rg.location
-  resource_group_name = azurerm_resource_group.terraform_app_service_rg.name
+  location            = azurerm_resource_group.terraform_appservice_rg.location
+  resource_group_name = azurerm_resource_group.terraform_appservice_rg.name
   service_plan_id     = azurerm_service_plan.terraform_app_service_plan.id
   site_config {}
 }
