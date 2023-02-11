e7d3fe5aca80414e287f6e46739b0bfd2d55e44a
SudhakarVSBattula <sudhakarbvs@outlook.com>
2023-02-11 21:50:03 +0530

    changed app plan name

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "terraform_appservice_rg" {
-  name     = "terraform-appservice-rg"
+resource "azurerm_resource_group" "terraformappservice_rg" {
+  name     = "terraformappservice-rg"
   location = var.location
 }
 
-resource "azurerm_service_plan" "terraform_appservice_plan" {
-  name                = "terraform-appservice-plan"
-  location            = azurerm_resource_group.terraform_appservice_rg.location
-  resource_group_name = azurerm_resource_group.terraform_appservice_rg.name
+resource "azurerm_service_plan" "terraformappservice_plan" {
+  name                = "terraformappservice-plan"
+  location            = azurerm_resource_group.terraformappservice_rg.location
+  resource_group_name = azurerm_resource_group.terraformappservice_rg.name
   os_type             = "Windows"
   sku_name            = "S1"
 }
 
-resource "azurerm_windows_web_app" "terraform_appservice" {
+resource "azurerm_windows_web_app" "terraformappservice" {
   name                = "app-service-9096"
-  location            = azurerm_resource_group.terraform_appservice_rg.location
-  resource_group_name = azurerm_resource_group.terraform_appservice_rg.name
-  service_plan_id     = azurerm_service_plan.terraform_appservice_plan.id
+  location            = azurerm_resource_group.terraformappservice_rg.location
+  resource_group_name = azurerm_resource_group.terraformappservice_rg.name
+  service_plan_id     = azurerm_service_plan.terraformappservice_plan.id
   site_config {}
 }
